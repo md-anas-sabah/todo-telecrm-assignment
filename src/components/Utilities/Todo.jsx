bbe38bda3bf868f2d5d12e419da6ba28299d6d7a
md-anas-sabah <mdanassabah@gmail.com>
2024-09-17 21:29:33 +0530

    Added time functionality

--- src/components/Utilities/Todo.jsx
+++ src/components/Utilities/Todo.jsx
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { FaRegEdit } from 'react-icons/fa';
-import { RiDeleteBin5Line } from 'react-icons/ri';
-import '../Styles/Todo.css';
-import { useTodos } from '../Providers/TodoProvider';
-import { useModal } from '../Providers/ModalProvider';
-import { useTheme } from '../Providers/ThemeProvider';
-import { Link } from 'react-router-dom';
-import { todoremoved } from './toasts';
+import React, { useState, useEffect } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import "../Styles/Todo.css";
+import { useTodos } from "../Providers/TodoProvider";
+import { useModal } from "../Providers/ModalProvider";
+import { useTheme } from "../Providers/ThemeProvider";
+import { todoremoved } from "./toasts";
 
 const Todo = ({ todo: { id, title, desc, tags, alloted, createdAt } }) => {
   const { theme } = useTheme();
   const { dispatch } = useTodos();
   const { modalOpen, setModalOpen } = useModal();
-  const [themeClass, setThemeClass] = useState('');
+  const [themeClass, setThemeClass] = useState("");
 
   const deleteTodo = (e) => {
     dispatch({
-      type: 'DELETE_TODO',
+      type: "DELETE_TODO",
       payload: id,
     });
     todoremoved(theme);
@@ -31,7 +30,7 @@ const Todo = ({ todo: { id, title, desc, tags, alloted, createdAt } }) => {
         todoTitle: title,
         todoDesc: desc,
         todoAlloted: alloted,
-        todoTags: tags.join(', '),
+        todoTags: tags.join(", "),
         todoCreatedAt: createdAt,
       },
       status: true,
@@ -39,29 +38,28 @@ const Todo = ({ todo: { id, title, desc, tags, alloted, createdAt } }) => {
   };
 
   useEffect(() => {
-    setThemeClass(theme === 'dark' ? '' : 'light-theme');
+    setThemeClass(theme === "dark" ? "" : "light-theme");
   }, [theme]);
 
   return (
     <div
       className={`todo flex flex-col align-start justify-center ${themeClass}`}
     >
-      <Link to={`/todo/${id}`} className='todo-title'>
-        {title}
-      </Link>
+      <h1 className="title">{title}</h1>
+
       {desc && (
-        <p className='todo-desc'>
+        <p className="todo-desc">
           {desc.charAt(0).toUpperCase() + desc.slice(1)}
         </p>
       )}
-      <div className='todo-tags flex-row-wrap align-center justify-start'>
+      <div className="todo-tags flex-row-wrap align-center justify-start">
         <h5>TAGS: </h5>
-        {tags.filter((tag) => tag !== '').length > 0 ? (
+        {tags.filter((tag) => tag !== "").length > 0 ? (
           tags.map((tag, idx) => (
-            <div key={idx} className='tag'>
+            <div key={idx} className="tag">
               <h5>
-                <span className='apos'>❛</span> {tag}{' '}
-                <span className='apos'>❜</span>
+                <span className="apos">❛</span> {tag}{" "}
+                <span className="apos">❜</span>
               </h5>
             </div>
           ))
@@ -69,9 +67,10 @@ const Todo = ({ todo: { id, title, desc, tags, alloted, createdAt } }) => {
           <p>None</p>
         )}
       </div>
-      <div className='todo-icons flex-center'>
-        <FaRegEdit className='icon icon-edit' onClick={editTodo} />
-        <RiDeleteBin5Line className='icon icon-delete' onClick={deleteTodo} />
+      <div> {alloted ? `Time Alloted: ${alloted}mins` : ""} </div>
+      <div className="todo-icons flex-center">
+        <FaRegEdit className="icon icon-edit" onClick={editTodo} />
+        <RiDeleteBin5Line className="icon icon-delete" onClick={deleteTodo} />
       </div>
     </div>
   );
